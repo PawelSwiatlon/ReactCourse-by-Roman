4ad12ac414b227196a0196198364f396d14a488b
PawelSwiatlon <pswiatlon02@gmail.com>
2021-01-12 16:52:13 +0100

    Renderowanie warunkowe

---
+++ 02 - 01 - Przygotowanie pierwszy komponentów/src/components/title/title.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './title.module.scss';
+
+const Title = ({children}) => (
+    <h2 className={styles.title}>{children}</h2>
+);
+
+export default Title;
